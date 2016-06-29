451d757e5ce46ab9d3ef8bee43d2190e02afe8f7
Comaxy <comaxy@126.com>
2016-06-29 18:30:50 +0800

    Add send login data

--- SuperBrain/LoadingScene.swift
+++ SuperBrain/LoadingScene.swift
@@ -19,15 +19,25 @@ class GameScene: SKScene {
         self.addChild(myLabel)
         
         let queue = dispatch_queue_create("connect", DISPATCH_QUEUE_SERIAL)
-        dispatch_async(queue) { 
-            let client = TCPClient(addr: "192.168.1.108", port: 7062)
+        dispatch_async(queue) {
+            let ip = "172.16.160.12"
+            // let ip = "192.168.1.108"
+            let client = TCPClient(addr: ip, port: 7062)
             let result = client.connect(timeout: 10)
             dispatch_async(dispatch_get_main_queue(), { 
                 if !result.0 {
                     myLabel.text = "Load failed!"
                 } else {
-                    let friendListScene = FriendListScene(size: self.size)
-                    self.view?.presentScene(friendListScene)
+//                    let playerInfo = "流星雨;123456";
+//                    let playerInfoData = playerInfo.dataUsingEncoding(NSUTF8StringEncoding)
+//                    client.send(data: [UInt8(1)])
+//                    let length = NSData(bytes: playerInfoData?.length, length: 2)
+//                    client.send(data: [UInt8((playerInfoData?.length)!)])
+//                    var data = NSMutableData();
+//                    var p = UnsafePointer<Void>();
+ 
+                    //let friendListScene = FriendListScene(size: self.size)
+                    //self.view?.presentScene(friendListScene)
                 }
             })
         }
