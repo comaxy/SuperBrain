755b5aeea235dd3da575d3234e591336a3b5259a
老公 <comaxy@126.com>
2016-07-02 16:58:35 +0800

    修改登录逻辑

--- SuperBrain/GameViewController.swift
+++ SuperBrain/GameViewController.swift
@@ -14,33 +14,51 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let userDefaults = NSUserDefaults.standardUserDefaults()
-        let username = userDefaults.objectForKey("username")
-        if username != nil {
-            let skView = self.view as! SKView
-            let scene = LoadingScene(size: skView.bounds.size)
-            skView.presentScene(scene)
-            
-        } else {
-            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let registerViewController = mainStoryboard.instantiateViewControllerWithIdentifier("registerViewController")
-            self.navigationController?.presentViewController(registerViewController, animated: false, completion: nil)
-        }
+        let skView = self.view as! SKView
+        let scene = LoadingScene(size: skView.bounds.size)
+        skView.presentScene(scene)
         
-//        if let scene = GameScene(fileNamed:"GameScene") {
-//            // Configure the view.
-//            let skView = self.view as! SKView
-//            skView.showsFPS = true
-//            skView.showsNodeCount = true
-//            
-//            /* Sprite Kit applies additional optimizations to improve rendering performance */
-//            skView.ignoresSiblingOrder = true
-//            
-//            /* Set the scale mode to scale to fit the window */
-//            scene.scaleMode = .AspectFill
-//            
-//            skView.presentScene(scene)
-//        }
+        scene.label.text = "正在连接服务器..."
+        let queue = dispatch_queue_create("socket_queue", DISPATCH_QUEUE_SERIAL)
+        
+        dispatch_async(queue) {
+            
+            // connect to server
+            let ip = "172.16.160.12"
+            //let ip = "192.168.1.108"
+            let client = TCPClient(addr: ip, port: 7062)
+            let result = client.connect(timeout: 10)
+
+            if !result.0 {
+                dispatch_sync(dispatch_get_main_queue(), {
+                    scene.label.text = "连接服务器失败，请稍后再试！"
+                })
+                return;
+            }
+            
+            let userDefaults = NSUserDefaults.standardUserDefaults()
+            let username = userDefaults.objectForKey("username")
+            if username != nil {
+                dispatch_sync(dispatch_get_main_queue(), { 
+                    scene.label.text = "正在登录..."
+                })
+                let data = NSMutableData()
+                let eventId = UnsafeMutablePointer<UInt8>.alloc(1)
+                eventId.initialize(SockEvent.REGISTER.rawValue)
+                data.appendBytes(eventId, length: 1)
+                let playerInfo = (username as! String) + ";" + (userDefaults.objectForKey("password") as! String);
+                let playerInfoData = playerInfo.dataUsingEncoding(NSUTF8StringEncoding)
+                let bodyLength = UnsafeMutablePointer<UInt16>.alloc(1)
+                bodyLength.initialize(UInt16((playerInfoData?.length)!))
+                data.appendBytes(bodyLength, length: 2)
+                data.appendData(playerInfoData!)
+                client.send(data: data)
+            } else {
+                let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let registerViewController = mainStoryboard.instantiateViewControllerWithIdentifier("registerViewController")
+                self.presentViewController(registerViewController, animated: false, completion: nil)
+            }
+        }
     }
 
     override func shouldAutorotate() -> Bool {
@@ -57,7 +75,6 @@ class GameViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
     override func prefersStatusBarHidden() -> Bool {
--- SuperBrain/LoadingScene.swift
+++ SuperBrain/LoadingScene.swift
@@ -1,49 +1,13 @@
-//
-//  GameScene.swift
-//  SuperBrain
-//
-//  Created by thunisoft on 16/6/28.
-//  Copyright (c) 2016年 cynhard. All rights reserved.
-//
-
 import SpriteKit
 
 class LoadingScene: SKScene {
+    
+    let label = SKLabelNode(fontNamed:"Chalkduster")
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Loading..."
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
-        
-        let queue = dispatch_queue_create("connect", DISPATCH_QUEUE_SERIAL)
-        dispatch_async(queue) {
-            let ip = "172.16.160.12"
-            //let ip = "192.168.1.108"
-            let client = TCPClient(addr: ip, port: 7062)
-            let result = client.connect(timeout: 10)
-            dispatch_async(dispatch_get_main_queue(), { 
-                if !result.0 {
-                    myLabel.text = "Load failed!"
-                } else {
-                    let userDefaults = NSUserDefaults.standardUserDefaults()
-                    let username = userDefaults.objectForKey("username") as! String
-                    let password = userDefaults.objectForKey("password") as! String
-                    let data = NSMutableData()
-                    let eventId = UnsafeMutablePointer<UInt8>.alloc(1)
-                    eventId.initialize(SockEvent.REGISTER.rawValue)
-                    data.appendBytes(eventId, length: 1)
-                    let playerInfo = username + ";" + password;
-                    let playerInfoData = playerInfo.dataUsingEncoding(NSUTF8StringEncoding)
-                    let bodyLength = UnsafeMutablePointer<UInt16>.alloc(1)
-                    bodyLength.initialize(UInt16((playerInfoData?.length)!))
-                    data.appendBytes(bodyLength, length: 2)
-                    data.appendData(playerInfoData!)
-                    client.send(data: data)
-                }
-            })
-        }
+        self.label.text = "正在加载..."
+        self.label.fontSize = 20
+        self.label.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        self.addChild(label)
     }
 }
