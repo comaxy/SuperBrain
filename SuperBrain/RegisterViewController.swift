781a677f9b3d84a91556abae65032ea21dba77fa
老公 <comaxy@126.com>
2016-07-02 15:02:57 +0800

    完成注册界面

--- SuperBrain/RegisterViewController.swift
+++ SuperBrain/RegisterViewController.swift
@@ -1,22 +1,58 @@
-//
-//  RegisterViewController.swift
-//  SuperBrain
-//
-//  Created by Theresa on 16/6/30.
-//  Copyright © 2016年 cynhard. All rights reserved.
-//
-
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    @IBOutlet weak var playerNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordAgainTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
     @IBAction func playerNameTextField_DidEndOnExit(sender: UITextField) {
+        self.passwordTextField.becomeFirstResponder()
     }
+    
     @IBAction func passwordTextField_DidEndOnExit(sender: UITextField) {
+        self.passwordAgainTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func passwordAgainTextField_DidEndOnExit(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func view_TouchDown(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func registerButton_TouchUpInside(sender: AnyObject) {
+        let playerName = self.playerNameTextField.text!.stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: " "))
+        if playerName.isEmpty {
+            self.alertMessage("用户名不能为空！")
+        }
+        
+        let password = self.passwordTextField.text!
+        if password.isEmpty {
+            self.alertMessage("密码不能为空！")
+        }
+        
+        let passwordAgain = self.passwordAgainTextField.text!
+        if passwordAgain.isEmpty {
+            self.alertMessage("确认密码不能为空！")
+        }
+        
+        if password != passwordAgain {
+            self.alertMessage("两次密码输入不一致，请重新输入！")
+        }
+        
+        
+    }
+    
+    func alertMessage(msg: String) {
+        let alert = UIAlertController(title: "提示", message: msg, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "确定", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 }
